aaff8493e3bb54e61b69251243adfd5b2c61addb
Amandeep Singh <programmeramandeep@gmail.com>
2023-08-07 18:34:51 +0530

    Jest assertions and matchers

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# jest-testing-with-typescript-nodejs
+# Unit Testing for Typescript & NodeJs Developers with Jest
+
 Master unit testing with NodeJs, Typescript, Jest and React. Write top quality Typescript and NodeJs software with Jest
 
 ## What you'll learn
-  1. Basics of TypeScript unit testing with Jest
-  2. Integration tests with TypeScript
-  3. Test Driven Development (TDD) with Jest
-  4. Unit testing React with TypeScript and Jest
-  5. Write tests for error handling
-  6. Jest - the basics: hooks, assertions, matchers
-  7. Jest - the advanced parts: aliases, custom matchers, snapshots
-  8. Advanced unit testing techniques using Jest
-  9. CI/CD pipeline using Github Actions
+
+1. Basics of TypeScript unit testing with Jest
+2. Integration tests with TypeScript
+3. Test Driven Development (TDD) with Jest
+4. Unit testing React with TypeScript and Jest
+5. Write tests for error handling
+6. Jest - the basics: hooks, assertions, matchers
+7. Jest - the advanced parts: aliases, custom matchers, snapshots
+8. Advanced unit testing techniques using Jest
+9. CI/CD pipeline using Github Actions
--- src/test/Utils.spec.ts
+++ src/test/Utils.spec.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from '../app/Utils';
+import { getStringInfo, toUpperCase } from '../app/Utils';
 
 describe('Utils test suite', () => {
   it('should resturn uppercase of valid string', () => {
@@ -9,5 +9,36 @@ describe('Utils test suite', () => {
     const actual = sut('abc');
     // assert
     expect(actual).toBe(expected);
+  });
+
+  it.only('should return info for valid string', () => {
+    const actual = getStringInfo('My-String');
+
+    expect(actual.lowerCase).toBe('my-string');
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      'M',
+      'y',
+      '-',
+      'S',
+      't',
+      'r',
+      'i',
+      'n',
+      'g',
+    ]);
+    expect(actual.characters).toContain<string>('M');
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+    );
+
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeTruthy();
+    expect(actual.extraInfo).toBeDefined();
   });
 });
