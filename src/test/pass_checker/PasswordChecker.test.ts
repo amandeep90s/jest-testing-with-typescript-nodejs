0fe14a5d007825528459033b03273c8867ef456f
Amandeep Singh <programmeramandeep@gmail.com>
2023-08-10 20:38:05 +0530

    Password checker iteration 1

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -7,7 +7,39 @@ describe('PasswordChecker test suite', () => {
     sut = new PasswordChecker();
   });
 
-  it('should do nothing for the moment', () => {
-    sut.checkPassword();
+  it('Password with less than 8 characters is invalid', () => {
+    const input = '1234567';
+    const actual = sut.checkPassword(input);
+    expect(actual).toBe(false);
+  });
+
+  it('Password with more than 8 characters is OK', () => {
+    const input = '12345678Aa';
+    const actual = sut.checkPassword(input);
+    expect(actual).toBe(true);
+  });
+
+  it('Password with no uppercase letter is invalid', () => {
+    const input = '1234abcd';
+    const actual = sut.checkPassword(input);
+    expect(actual).toBe(false);
+  });
+
+  it('Password with uppercase letter is valid', () => {
+    const input = '1234abcdA';
+    const actual = sut.checkPassword(input);
+    expect(actual).toBe(true);
+  });
+
+  it('Password with no lowercase letter is invalid', () => {
+    const input = '1234ABCD';
+    const actual = sut.checkPassword(input);
+    expect(actual).toBe(false);
+  });
+
+  it('Password with lowercase letter is valid', () => {
+    const input = '1234ABCDa';
+    const actual = sut.checkPassword(input);
+    expect(actual).toBe(true);
   });
 });
