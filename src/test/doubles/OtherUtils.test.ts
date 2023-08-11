fb53bb900793cc369ebfa309c1eddb747c20a955
Amandeep Singh <programmeramandeep@gmail.com>
2023-08-11 08:44:55 +0530

    Implementing stubs

---
+++ src/test/doubles/OtherUtils.test.ts
@@ -0,0 +1,15 @@
+import { calculateComplexity } from '../../app/doubles/OtherUtils';
+
+describe('OtherUtils test suite', () => {
+  it('calculates complexity', () => {
+    const someInfo = {
+      length: 5,
+      extraInfo: {
+        field1: 'someInfo',
+        field2: 'someOtherInfo',
+      },
+    };
+    const actual = calculateComplexity(someInfo as any);
+    expect(actual).toBe(10);
+  });
+});
