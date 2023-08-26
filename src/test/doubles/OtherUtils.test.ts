0be209d566303395db373edcfb5499f468b2b889
Amandeep Singh <programmeramandeep@gmail.com>
2023-08-26 20:00:46 +0530

    Mock modules

--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -4,7 +4,7 @@ import {
   toUpperCaseWithCb,
 } from '../../app/doubles/OtherUtils';
 
-describe('OtherUtils test suite', () => {
+describe.skip('OtherUtils test suite', () => {
   describe('OtherStringUtils tests with spies', () => {
     let sut: OtherStringUtils;
 
