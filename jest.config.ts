85f9f123282ce1ac4005e49f17f99a5f92c94f5d
Amandeep Singh <programmeramandeep@gmail.com>
2023-08-31 19:06:24 +0530

    Setup tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,15 @@
 import type { Config } from '@jest/types';
 
 const baseDir = '<rootDir>/src/app/server_app';
-const baseTestDir = '<rootDir>/src/test/server_app';
+const baseTestDir = '<rootDir>/src/test/server_app2';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [`${baseDir}/**/*.ts`],
-  testMatch: [`${baseTestDir}/**/*.ts`],
+  testMatch: [`${baseTestDir}/**/*test.ts`],
 };
 
 export default config;
