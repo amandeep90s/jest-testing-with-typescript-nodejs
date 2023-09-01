c9c1dd43b68e4a2a0229bcdb2c0d134ebf6e0b10
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-01 09:33:00 +0530

    Integration tests setup

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from '@jest/types';
 
 const baseDir = '<rootDir>/src/app/server_app';
-const baseTestDir = '<rootDir>/src/test/server_app';
+const baseTestDir = '<rootDir>/src/test/server_app3';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [`${baseDir}/**/*.ts`],
   testMatch: [`${baseTestDir}/**/*test.ts`],
 };
