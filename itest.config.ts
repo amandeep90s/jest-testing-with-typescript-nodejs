a67fcebd51df4db5c86f42ece464b9f49685ec88
Amandeep Singh <programmeramandeep@gmail.com>
2023-09-02 16:28:18 +0530

    Modified the configuration for itest

--- itest.config.ts
+++ itest.config.ts
@@ -7,7 +7,7 @@ const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   verbose: true,
-  collectCoverage: false,
+  collectCoverage: true,
   collectCoverageFrom: [`${baseDir}/**/*.ts`],
   testMatch: [`${baseTestDir}/**/*test.ts`],
   setupFiles: ['<rootDir>/src/test/server_app3/utils/config.ts'],
